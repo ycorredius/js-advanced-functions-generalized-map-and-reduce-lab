55745f2996f6b1fda005da343ed4bd3daa472a4f
shaun <ycorredius@gmail.com>
2020-04-05 14:44:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(source,functionName){
+    let result = [];
+    for(let i=0;i<source.length;i++){
+        result.push(functionName(source[i]));
+    }
+    return result;
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r);
+    }
+  
+    return r;
+  }
